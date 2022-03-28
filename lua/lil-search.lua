b939fed1732afc30cb256983ae42d2e87214b3ef
mvllow <mvllow@icloud.com>
2022-03-28 00:35:49 -0400

    remove `telescope.nvim`

--- lua/lil-search.lua
+++ lua/lil-search.lua
@@ -3,44 +3,6 @@
 
 --- Setup search.
 
-local use = require('lil-helpers').use
-
-use({
-	'nvim-telescope/telescope.nvim',
-	requires = 'nvim-lua/plenary.nvim',
-	config = function()
-		local opts = { silent = true }
-		vim.keymap.set('n', '<leader>f', ':Telescope find_files<cr>', opts) -- search files
-		vim.keymap.set('n', '<leader>/', ':Telescope live_grep<cr>', opts) -- search text
-
-		require('telescope').setup({
-			defaults = {
-				layout_config = {
-					horizontal = {
-						-- Increase preview width
-						preview_width = 0.6,
-					},
-				},
-			},
-			pickers = {
-				find_files = {
-					theme = 'dropdown',
-					previewer = false,
-					find_command = {
-						'fd',
-						'--type',
-						'f',
-						'--hidden',
-						'--exclude',
-						'.git',
-						'--strip-cwd-prefix',
-					},
-				},
-			},
-		})
-	end,
-})
-
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
