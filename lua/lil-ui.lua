060665d93e21fdc160c3ffca96be43250c777010
not <mvllow@icloud.com>
2022-04-13 11:25:23 -0400

    remove deprecated config

--- lua/lil-ui.lua
+++ lua/lil-ui.lua
@@ -10,7 +10,8 @@ use({
 	run = ':TSUpdate',
 	config = function()
 		require('nvim-treesitter.configs').setup({
-			ensure_installed = 'maintained',
+			ensure_installed = 'all',
+			ignore_install = { 'phpdoc' },
 			highlight = { enable = true },
 		})
 	end,
