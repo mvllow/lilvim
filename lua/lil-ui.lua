09d71ee89c4b42078b5b93ef678289d3909b444f
mvllow <mvllow@icloud.com>
2022-12-10 22:18:07 -0500

    install all treesitter parsers

--- lua/lil-ui.lua
+++ lua/lil-ui.lua
@@ -11,7 +11,6 @@ use({
 	config = function()
 		require('nvim-treesitter.configs').setup({
 			ensure_installed = 'all',
-			ignore_install = { 'phpdoc' },
 			highlight = { enable = true },
 		})
 	end,
