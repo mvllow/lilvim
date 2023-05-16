0050908bcfbb2aed42c2b95a60b01053f48fbc80
mvllow <mvllow@icloud.com>
2023-05-16 16:04:57 +0200

    remove colors and ftplugin
    This commit removes uncommon ftplugin overrides and an unused colorscheme.

--- ftplugin/svg.lua
+++
@@ -1,7 +0,0 @@
---- svg.lua
---- https://github.com/mvllow/lilvim
----
---- Apply svg language settings.
-
--- Override svg filetype for improved syntax highlighting.
-vim.opt_local.ft = 'html'
--- ftplugin/webc.lua
+++
@@ -1,7 +0,0 @@
---- webc.lua
---- https://github.com/mvllow/lilvim
----
---- Apply webc language settings.
-
--- Override webc filetype for improved syntax highlighting.
-vim.opt_local.ft = 'html'
