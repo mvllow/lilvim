f52dbdb467a9275e38a99fc9fa5688330c7c7df2
mvllow <mvllow@icloud.com>
2023-05-16 17:23:13 +0200

    update to latest nvim-tree api

--- lua/lil-extras.lua
+++ lua/lil-extras.lua
@@ -23,45 +23,16 @@ use({
 	"kyazdani42/nvim-tree.lua",
 	config = function()
 		require("nvim-tree").setup({
-			actions = {
-				open_file = {
-					quit_on_open = true,
-				},
-			},
-			filters = {
-				-- Hide ".git" folder.
-				custom = { "^.git$" },
-			},
-			git = {
-				-- Do not hide gitignored files.
-				ignore = false,
-			},
-			renderer = {
-				icons = {
-					-- Do not show tree icons.
-					show = {
-						file = false,
-						folder = false,
-						folder_arrow = false,
-						git = false,
-					},
-				},
-			},
-			trash = {
-				-- Use cross-platform trash command.
-				-- https://github.com/sindresorhus/trash-cli
-				cmd = "trash",
-			},
-			view = {
-				mappings = {
-					list = {
-						-- Replace destructive default with trash command.
-						{ key = "d", action = "trash" },
-						{ key = "D", action = "remove" },
-					},
-				},
-				side = "right",
-			},
+			on_attach = function(bufnr)
+				local api = require("nvim-tree.api")
+				api.config.mappings.default_on_attach(bufnr)
+				vim.keymap.set("n", "d", api.fs.trash, { buffer = bufnr })
+			end,
+			actions = { open_file = { quit_on_open = true } },
+			git = { ignore = false },
+			renderer = { icons = { show = { file = false, folder = false, folder_arrow = false, git = false } } },
+			trash = { cmd = "trash" },
+			view = { side = "right" },
 		})
 	end,
 })
