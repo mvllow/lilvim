e1a4c777c4f116f0cb75bd9af82676dd8642d5e1
mvllow <mvllow@icloud.com>
2025-12-04 11:26:15 -0600

    feat(lil-stats): use buffer-local variables for storing state

--- plugin/lilvim.lua
+++ plugin/lilvim.lua
@@ -1,3 +1,13 @@
+--- lil-stats
+vim.api.nvim_create_autocmd({ "BufEnter", "TextChanged", "TextChangedI" }, {
+	group = vim.api.nvim_create_augroup("LilStatsFile", { clear = true }),
+	callback = function()
+		LilStats = require("lil-stats")
+		LilStats.get_file_stats()
+	end
+})
+
+--- lil-toggle
 vim.api.nvim_create_user_command("LilToggle", function(opts)
 	LilToggle = require("lil-toggle")
 	LilToggle.toggle(opts.args)
